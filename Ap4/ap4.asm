43cb51092674cc88c5965304f0cf2d32730a5fc7
Sergio Pessoa <33792056+sslp23@users.noreply.github.com>
2019-05-30 16:57:04 -0300

    ta funcionando com o limite até 7

--- Ap4/ap4.asm
+++ Ap4/ap4.asm
@@ -1,77 +1,77 @@
 extern printf
 
 section .data
-    limite equ 1000
-    i db 0 
-    um dw 1
-    ask: db 'Calculadora de Pi pela Serie de Taylor!', 0
+	limite: equ 7
+	ask: db 'Calculadora de Pi pela Serie de Taylor!', 0
     answer: db 'O valor achado foi: ', 0
-    fmt:    db "%s", 10, 0 ; printf format string follow by a newline(10) and a null terminator(0), "\n",'0'
-    fmt2: db "%f", 13, 10, 0
-    integer: db "%d", 10, 0
+    string:    db "%s", 10, 0
+    integer: dd "%d", 0xA, 0
+    double: db "%f", 0xA, 0
+    four: dd 4.0
+    um: dd 1
+    menosum: dd -1	
+
 section .bss
-    valor resd 1
-    result resd 1
-    %macro leibniz 2   
-        shl %1, 1; multiplica o valor de eax por 2 => eax <- 2*eax
-        mov dword[valor], %1; move valor de eax (2*i) na memória
-        fld dword[valor]; coloca valor (2*i) na pilha
+	result: resd 1
+	aux: resd 1
+	sum: resd 1
+	i: resd 1
 
-        fld1; adiciona o valor (1) à pilha
-        faddp; soma valor de st0 com st1 e salva em st0 => st0 <- (2*i+1)
-
-        mov dword[valor], %2; move valor de edx (1 ou -1) para memória
-        fld dword[valor]; coloca valor (-1 ou 1) na pilha
-        
-        fdivrp; st0 <- ((±1) / (2*i + 1))
-        fst dword[valor]; salva o valor na posição de memória de result
-
-        mov eax, dword[valor]; Comprime o valor de 64 bits para 32 bits e move para eax
-        add dword[result], eax; usa o endereço de result como acumulador
-    
-    %endmacro
-
-;daqui pra baixo, TOP (sergio)
 section .text
-    global main
-main:
-    finit; Inicializa a pilha
-    push ask
-    push fmt
-    call printf
-    add esp, 8
-    mov ecx, limite; quantidade de iterações 
-    mov dword[result], 0 ;Seta o valor de result para 0
+	global main
 
-    leibniz_serie:
-        mov eax, limite ; 
-        sub eax, ecx; eax(valor de i) <- eax - ecx(contador)
-        
-        mov ebx, eax; passa valor de eax para ebx
+main:
+	finit
+	push ask
+	push string
+	call printf
+	add esp, 8
+	mov ecx, limite ;quantidade de iterações
+	mov dword[sum], 0
+	mov edx, 0
+
+	serie:
+		;mov eax, 11	
+		mov eax, limite
+		sub eax, ecx; eax(valor de i) <- eax - ecx(contador)
+
+		mov ebx, eax; passa valor de eax para ebx
         and ebx, 1 ;ve se o ultimo bit do valor de i eh igual 1
         cmp ebx, 1 ;se for igual 1, eh impar
         je set_i_neg ;se for impar, o numerador eh igual -1
 
-        mov edx, 1
-        back: ;volta da negacao (se negou neh)
-            leibniz edx, eax ;chama o esgoto (macro)
-        loop leibniz_serie ;loop top
+        fild dword[um] ; st: +1
+        back:
+        	mov ebx, 2
+        	mul ebx ; eax = 2*i
+        	add eax, 1; eax = 2*i+1
+        	mov dword[aux], eax
+        	fild dword[aux]; st: (2*i+1), (+-1)
+        	fxch ; st: (+-1), (2*i+1)
+        	fdiv st0, st1; st: +-1/(2*i+1)
+        	fstp dword[i]
+        	fld dword[sum] ;st0 = sum
+        	fadd dword[i] ; st0 = sum + i (i eh o valor achado nessa iteração)
+        	fstp dword[sum] ;sum recebe o novo somatorio
+        	
+        	loop serie
 
-    jmp print_result
-    set_i_neg:
-        mov edx, 1
-        neg edx
+    jmp end
+   	
+   	set_i_neg:
+        fild dword[menosum] ; st= -1
+  		
         jmp back
-    
-    print_result:    
-        push answer
-        push fmt
-        call printf
-        add esp, 8
-        push dword[result]
-        push fmt2
-        call printf
-        add esp, 8
 
-    mov eax, 0
-ret        
+	end:
+
+		sub esp, 8 ;espaco na pilha pra um double
+		fld dword[sum] ;carrega o valor do somatorio
+		fmul dword[four] ;multiplica por 4 p pegar pi
+		fstp qword[esp] ;tira da pilha
+		push double ;passa o formato
+		call printf ;printa em c
+		add esp, 12
+
+	mov eax, 0
+	ret
